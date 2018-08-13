fc92c4430435555ee1ab984af1778196b4d29b36
Marian Capote <mcapote250390@gmail.com>
2018-08-13 09:08:13 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = 'Marian';
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI = Math.PI;
+  a = 0.234;
+  money = 1234.5;
 }
